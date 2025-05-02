59a8e60e7f6d0ee3216a3030832d36a7ea1e7074
SemRithiro <rithiro@gmail.com>
2025-05-03 00:17:18 +0700

    update package.json and profile

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,7 @@ import Index from './views';
 
 export default function Router() {
 	return (
-		<BrowserRouter basename='/basename'>
+		<BrowserRouter basename='/curriculumn_vitae'>
 			<Routes>
 				<Route path='/' element={<Index />} />
 			</Routes>
